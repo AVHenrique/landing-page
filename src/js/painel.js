a154aacdaae96c0391afa30424d7d7c7a93a21c3
Henrique Andreotti Vicentini <127172450+AVHenrique@users.noreply.github.com>
2023-07-08 20:13:41 -0300

    Add files via upload

---
+++ src/js/painel.js
@@ -0,0 +1,42 @@
+const imagens = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+
+let imagemAtual = 0;
+
+function esconderImagem(){
+    imagens.forEach(imagem =>{
+        imagem.classList.remove('mostrar')
+    });
+}
+
+function mostrarImagem(){
+    imagens[imagemAtual].classList.add('mostrar');
+}
+
+setaAvancar.addEventListener('click', function(){
+
+    const totalDeImagens = imagens.length -1;
+    if(imagemAtual === totalDeImagens){
+        return;
+    }
+
+    imagemAtual++;
+
+    esconderImagem();
+
+    mostrarImagem();
+});
+
+setaVoltar.addEventListener('click', function(){
+    
+    if(imagemAtual === 0) {
+        return;
+    }
+    
+    imagemAtual--;
+
+    esconderImagem();
+
+    mostrarImagem();
+})
